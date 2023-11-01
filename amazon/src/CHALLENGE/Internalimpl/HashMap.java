5cc03db15c3438a9e43eef5c251ed7579e8d4b39
krashokkumarnaidu@gmail.com <krashokkumarnaidu@gmail.com>
2023-11-01 11:06:31 +0530

    day 28

--- amazon/src/CHALLENGE/Internalimpl/HashMap.java
+++ amazon/src/CHALLENGE/Internalimpl/HashMap.java
@@ -64,7 +64,7 @@ class Main {
     public static void main(String[] args) {
         HashMap<String, Integer> map = new HashMap<>();
         map.put("one", 1);
-        map.put("two", 2);a
+        map.put("two", 2);
         map.put("three", 3);
 
         System.out.println("Size: " + map.size());
